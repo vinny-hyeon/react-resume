539e3e1a9ba6a73eafffddb900e6b92ef706b9b7
bini0823 <bini0823@cau.ac.kr>
2021-01-17 02:22:47 +0900

    소개 페이지 구현 및 소개 글 작성

---
+++ src/myInfo/_intro.js
@@ -0,0 +1,50 @@
+export const myInfos = [
+  {
+    label: "School",
+    data: "중앙대학교(2015.03 ~ )",
+  },
+  {
+    label: "Major",
+    data: "컴퓨터공학",
+  },
+  {
+    label: "Email",
+    data: "bini0823@cau.ac.kr",
+  },
+  {
+    label: "Major GPA",
+    data: "3.81 / 4.5",
+  },
+  {
+    label: "Hobby",
+    data: "Soccer",
+  },
+  {
+    label: "Introduce",
+    data:
+      "긍정적이고 좋아하는 일은 하루종일 할 정도로 열심히합니다. 다양한 스포츠를 잘하고 좋아합니다. 똑똑하고 좋은 분들과 함께 일하며 배워보고 싶습니다~",
+  },
+];
+
+export const linkData = [
+  {
+    domain: "github",
+    address: "https://github.com/bini0823",
+    iconName: "github",
+  },
+  {
+    domain: "facebook",
+    address: "https://www.facebook.com/hyunbin.kwon.3/",
+    iconName: "facebook",
+  },
+  {
+    domain: "google",
+    address: "",
+    iconName: "google plus",
+  },
+  {
+    domain: "blog",
+    address: "https://blog.naver.com/bini0823",
+    iconName: "blogger",
+  },
+];
