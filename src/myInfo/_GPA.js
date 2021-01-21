8179e7cc63858402348c67253f4d2014dc18db31
bini0823 <bini0823@cau.ac.kr>
2021-01-21 20:26:22 +0900

    내 학점에 관한 정보 myInfo에 추가

---
+++ src/myInfo/_GPA.js
@@ -0,0 +1,203 @@
+export const scoreTable = {
+  "A+": 4.5,
+  A: 4,
+  "B+": 3.5,
+  B: 3,
+  "C+": 2.5,
+  C: 2,
+  "D+": 1.5,
+  D: 1,
+  F: 0,
+};
+
+export const gpa = [
+  {
+    semester: "1학년 1학기",
+    isRegister: true,
+    subject: [
+      {
+        name: "창의적설계",
+        weight: "3",
+        score: "A+",
+      },
+      {
+        name: "기초프로그래밍",
+        weight: "3",
+        score: "C+",
+      },
+      {
+        name: "일반물리학(1)",
+        weight: "2",
+        score: "A+",
+      },
+    ],
+  },
+  {
+    semester: "1학년 2학기",
+    isRegister: true,
+    subject: [
+      {
+        name: "이산수학",
+        weight: "3",
+        score: "A",
+      },
+      {
+        name: "논리회로",
+        weight: "3",
+        score: "B+",
+      },
+    ],
+  },
+  {
+    semester: "2학년 1학기",
+    isRegister: true,
+    subject: [
+      {
+        name: "미적분학",
+        weight: "3",
+        score: "A+",
+      },
+      {
+        name: "소프트웨어프로젝트",
+        weight: "3",
+        score: "B",
+      },
+      {
+        name: "자료구조",
+        weight: "3",
+        score: "B",
+      },
+      {
+        name: "컴퓨터시스템및어셈블리언어",
+        weight: "3",
+        score: "C+",
+      },
+    ],
+  },
+  {
+    semester: "2학년 2학기",
+    isRegister: true,
+    subject: [
+      {
+        name: "프로그래밍",
+        weight: "3",
+        score: "A+",
+      },
+      {
+        name: "확률및통계",
+        weight: "3",
+        score: "A+",
+      },
+      {
+        name: "오토마타와형식언어",
+        weight: "3",
+        score: "A+",
+      },
+      {
+        name: "컴퓨터구조",
+        weight: "3",
+        score: "B+",
+      },
+      {
+        name: "객체지향프로그래밍",
+        weight: "3",
+        score: "B+",
+      },
+    ],
+  },
+  {
+    semester: "3학년 1학기",
+    isRegister: true,
+    subject: [
+      {
+        name: "선형대수학",
+        weight: "3",
+        score: "A+",
+      },
+      {
+        name: "수치해석",
+        weight: "3",
+        score: "A",
+      },
+      {
+        name: "소프트웨어공학",
+        weight: "3",
+        score: "A",
+      },
+      {
+        name: "운영체제",
+        weight: "3",
+        score: "B+",
+      },
+      {
+        name: "컴파일러",
+        weight: "3",
+        score: "B+",
+      },
+      {
+        name: "알고리즘",
+        weight: "3",
+        score: "B",
+      },
+      {
+        name: "정보보호이론",
+        weight: "3",
+        score: "C+",
+      },
+    ],
+  },
+  {
+    semester: "3학년 2학기",
+    isRegister: true,
+    subject: [
+      {
+        name: "컴퓨터통신",
+        weight: "3",
+        score: "A+",
+      },
+      {
+        name: "데이터베이스설계",
+        weight: "3",
+        score: "A+",
+      },
+      {
+        name: "컴퓨터그래픽스",
+        weight: "3",
+        score: "A+",
+      },
+      {
+        name: "캡스톤디자인",
+        weight: "3",
+        score: "A",
+      },
+      {
+        name: "인공지능",
+        weight: "3",
+        score: "A",
+      },
+      {
+        name: "영상처리",
+        weight: "3",
+        score: "A",
+      },
+      {
+        name: "프로그래밍언어론",
+        weight: "3",
+        score: "A",
+      },
+      {
+        name: "(계절)오픈소스SW와 파이썬",
+        weight: "3",
+        score: "A+",
+      },
+    ],
+  },
+  {
+    semester: "4학년 1학기",
+    isRegister: false,
+  },
+  {
+    semester: "4학년 2학기",
+    isRegister: false,
+  },
+];
