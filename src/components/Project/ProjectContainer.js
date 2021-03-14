2a759abfa8ebca26d4f0e6ba810a5dc18fc4206a
bini0823 <bini0823@cau.ac.kr>
2021-03-14 23:48:27 +0900

    project 페이지 모바일 재스타일링

--- src/components/Project/ProjectContainer.js
+++ src/components/Project/ProjectContainer.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Project from "./Project";
 import styled from "styled-components";
+import { Mobile, PC } from "../../hook/mediaQuery";
+import { mobileStyle } from "./style/mobile";
 
 const StyleContainer = styled.div`
   position: relative;
@@ -14,11 +16,20 @@ const StyleContainer = styled.div`
 function ProjectContainer(props) {
   return (
     <div>
-      <StyleContainer>
-        <h1>Project</h1>
-        <Project focusTarget={props.focusTarget} tech="ReactNative" />
-        <Project focusTarget={props.focusTarget} tech="React" />
-      </StyleContainer>
+      <PC>
+        <StyleContainer>
+          <h1>Project</h1>
+          <Project focusTarget={props.focusTarget} tech="ReactNative" />
+          <Project focusTarget={props.focusTarget} tech="React" />
+        </StyleContainer>
+      </PC>
+      <Mobile>
+        <mobileStyle.StyleContainer>
+          <h1>Project</h1>
+          <Project focusTarget={props.focusTarget} tech="ReactNative" />
+          <Project focusTarget={props.focusTarget} tech="React" />
+        </mobileStyle.StyleContainer>
+      </Mobile>
     </div>
   );
 }
