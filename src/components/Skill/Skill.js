56dcc6c70fc84b9fd45a4cdd0f9eb501af3bc2a1
hyeonbin.gwon <hyeonbin.gwon@hayanmind.comhyeonbin.gwon@hayanmind.comhyeonbin.gwon@hayanmind.com>
2021-04-17 18:20:31 +0900

    Add skill page

---
+++ src/components/Skill/Skill.js
@@ -0,0 +1,100 @@
+import React, { useState, useCallback, useMemo } from "react";
+import { Mobile, PC } from "../../hook/mediaQuery";
+import { skillTable, skillDescription } from "../../myInfo/_skill";
+import { Flag, Icon } from "semantic-ui-react";
+import { Progress } from "semantic-ui-react";
+import styled from "styled-components";
+
+const SkillContainer = styled.div`
+    display: flex;
+    flex: 1;
+    flex-direction: row;
+    .skill {
+        flex: 1;
+        flex-direction: column;
+    }
+    .description {
+        flex: 1;
+        flex-direction: column;
+        text-align: center;
+    }
+`;
+
+const SkillElementContainer = styled.div`
+    display: flex;
+    flex: 1;
+    flex-direction: row;
+    align-items: center;
+`
+
+const IndexContainer = styled.div`
+    .icon {
+        color: gray;
+    }
+    .selectedIcon {
+        color: red;
+    }
+`
+
+const ProgressContainer = styled.div`
+    flex: 1;
+    flex-direction: column;
+    .skillName {
+        font-weight: bold;
+    }
+`
+
+const ContentContainer = styled.div`
+    flex: 1;
+
+`
+
+const indexIcon = <Icon className="icon" name="arrow right"/>
+const selectedIcon = <Icon className="selectedIcon" name="arrow right" size="large"/>
+
+function Skill(props){
+    const [index, setIndex] = useState(0);
+    const [preIndex, setPreIndex] = useState(0);
+
+    const renderSkill = () => {
+        return skillTable.map((ele, idx) => {
+            return (
+            <SkillElementContainer onClick={() => setIndex(idx)} onMouseOver={() => setPreIndex(idx)} onMouseOut={() => setPreIndex(index)}>
+                <IndexContainer>
+                    {index === idx ? selectedIcon : indexIcon}
+                </IndexContainer>
+                <ProgressContainer>
+                    <div className="skillName" style={index === idx ? {} : {color:'gray'}}>{ele.skill}</div>
+                    <Progress
+                        value={ele.level}
+                        total="5"
+                        progress="ratio"
+                        size={index === idx || preIndex === idx ? "medium" : "small"}
+                        color={ele.color}
+                    />
+                </ProgressContainer>
+            </SkillElementContainer>)
+        })
+    }
+
+    const renderDescription = () => {
+        const skill = skillDescription[index];
+        return (
+            <ContentContainer>
+                <h1>{skill.title}</h1>
+                <p>{skill.content}</p>
+            </ContentContainer>
+        )
+    }
+
+    return(
+        <div>
+            <SkillContainer>
+                <div className="skill">{renderSkill()}</div>
+                <div className="description">{renderDescription()}</div>
+            </SkillContainer>
+        </div>
+    )
+}
+
+export default Skill;
